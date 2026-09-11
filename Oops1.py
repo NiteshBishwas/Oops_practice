fba1600026338df3c94736d0195ddc12723b8d23
Nitesh Bishwas <niteshbishwas.nbks00@gmail.com>
2026-09-11 21:15:20 +0530

    class and object basics done

---
+++ Oops1.py
@@ -0,0 +1,19 @@
+##intiate the class
+class employee:
+    def __init__(self):
+        print("started executing attributes/data")
+        self.id=124
+        self.salary=50000
+        self.designation = "SDE"
+        print("attributes/data have been initiated")
+
+    def travel(self,destination):
+        print("This travel function is called manually")
+        print(f"Employee is now traveling to {destination}")
+
+
+##Creating and obj/instance of the class
+charli=employee()
+# print(charli.salary)
+# print(charli.id)
+charli.travel("Bihar")
---
+++ rough.py
@@ -0,0 +1,14 @@
+lst=[1,2,3]
+my_str='mlops'
+my_int=155
+
+# print(type(lst))
+# print(type(my_str))
+
+
+lst.clear()
+print(lst)
+
+a=2
+b=3
+print(a+b)
